c4f127bcf3a5936928adbaffa61c6a8c6e3c2809
hl_tang <hltang@protonmail.com>
2024-02-24 21:48:46 +0900

    add cors & modify front-end: get request

--- server/server_django_ninja_version/arXiv_friends/settings.py
+++ server/server_django_ninja_version/arXiv_friends/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'arxiv_search_paper',
     'gpt_simplify',
     'recommendation',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173"
+]
