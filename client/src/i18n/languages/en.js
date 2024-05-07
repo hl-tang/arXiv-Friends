bf037f516f2f046f7a8a06c81ccfa707540209ea
hl_tang <hltang@protonmail.com>
2024-05-08 00:33:09 +0900

    modify: give hint that user should input English to search for arxiv paper, other language input contents will cause error

--- client/src/i18n/languages/en.js
+++ client/src/i18n/languages/en.js
@@ -1,13 +1,13 @@
 export default {
-    searchPapers: "Search papers",
-    login: "Log in",
-    recommendForYou: "Recommend for you",
-    searchResult: "Search result",
-    abstract: "abstract",
-    hide: "Hide",
-    show: "Show",
-    authors: "Authors",
-    published: "Published",
-    categories: "Categories",
-    download: "Download"
-}
+  searchPapers: "Search papers (please input English to search)",
+  login: "Log in",
+  recommendForYou: "Recommend for you",
+  searchResult: "Search result",
+  abstract: "abstract",
+  hide: "Hide",
+  show: "Show",
+  authors: "Authors",
+  published: "Published",
+  categories: "Categories",
+  download: "Download",
+};
--- client/src/i18n/languages/ja.js
+++ client/src/i18n/languages/ja.js
@@ -1,13 +1,13 @@
 export default {
-    searchPapers: "論文検索",
-    login: "ログイン",
-    recommendForYou: "貴方にお薦め",
-    searchResult: "検索結果",
-    abstract: "要旨",
-    hide: "隠す",
-    show: "表示する",
-    authors: "著者",
-    published: "リリース日付",
-    categories: "ジャンル",
-    download: "ダウンロードpdf"
-}
+  searchPapers: "論文検索(検索したい内容は英語で入力してください)",
+  login: "ログイン",
+  recommendForYou: "貴方にお薦め",
+  searchResult: "検索結果",
+  abstract: "要旨",
+  hide: "隠す",
+  show: "表示する",
+  authors: "著者",
+  published: "リリース日付",
+  categories: "ジャンル",
+  download: "ダウンロードpdf",
+};
