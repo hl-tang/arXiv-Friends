11463255f0a857a2e9e1461b82ae1a36c3ef9c6f
hl_tang <hltang@protonmail.com>
2024-05-17 20:35:31 +0900

    reorganize frontend

--- client/src/stores/omitAbstract.js
+++ client/src/stores/omitAbstract.js
@@ -1,7 +1,7 @@
-import { defineStore } from 'pinia'
-import { ref } from "vue"
-
-export const useOmitAbstractStore = defineStore('omit-abstract', () => {
-    const omitAbstract = ref(true)
-    return { omitAbstract }
-})
+import { defineStore } from "pinia";
+import { ref } from "vue";
+// 在home和PaperRow都用到
+export const useOmitAbstractStore = defineStore("omit-abstract", () => {
+  const omitAbstract = ref(true);
+  return { omitAbstract };
+});
